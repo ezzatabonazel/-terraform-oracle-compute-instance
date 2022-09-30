91e266e17bc2555d901789ed2fe32850a5041adb
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-09-30 20:54:13 +0200

    add outputs

---
+++ oci-terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_private_ips" {
+  value = [oci_core_instance.test_instance.*.private_ip]
+}
+
+output "instance_public_ips" {
+  value = [oci_core_instance.test_instance.*.public_ip]
+}
