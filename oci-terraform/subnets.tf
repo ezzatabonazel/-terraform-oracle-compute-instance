c58ec2c0f37eb2d38dc1fa93ff614946117bcc10
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-09-30 20:50:42 +0200

    subnet

---
+++ oci-terraform/subnets.tf
@@ -0,0 +1,16 @@
+resource "oci_core_subnet" "test_subnet" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  cidr_block          = "10.0.1.0/24"
+  display_name        = "TestSubnet"
+  dns_label           = "testsubnet"
+  security_list_ids   = [oci_core_vcn.test_vcn.default_security_list_id]
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_vcn.test_vcn.id
+  route_table_id      = oci_core_vcn.test_vcn.default_route_table_id
+  dhcp_options_id     = oci_core_vcn.test_vcn.default_dhcp_options_id
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
