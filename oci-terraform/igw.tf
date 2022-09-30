82143f22ddc837a7ee5de83fd606b46b7d7ae940
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-09-30 20:51:13 +0200

    internet gateway

---
+++ oci-terraform/igw.tf
@@ -0,0 +1,5 @@
+resource "oci_core_internet_gateway" "test_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = "TestInternetGateway"
+  vcn_id         = oci_core_vcn.test_vcn.id 
+}
